066c4d62954f585a5cf330ecbb9708d228724388
handuah <handuah@gmail.com>
2021-06-23 15:42:22 +0000

    added padding right

--- lib/screens/login-signup/login.dart
+++ lib/screens/login-signup/login.dart
@@ -17,142 +17,147 @@ class _LoginState extends State<Login> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Trippin',
-                style: GoogleFonts.lobster(
-                  textStyle: TextStyle(
-                    color: HexColor("#0b85ff"),
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+              left: screenWidth * 0.08,
+              right: screenWidth * 0.08,
+              top: screenHeight * 0.3),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Trippin',
+                  style: GoogleFonts.lobster(
+                    textStyle: TextStyle(
+                      color: HexColor("#0b85ff"),
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.16,
-              ),
-              Form(
-                child: Column(
-                  children: [
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(
-                          color: HexColor("#2c2c2c"),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
+                SizedBox(
+                  height: screenHeight * 0.16,
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
                             color: HexColor("#2c2c2c"),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: HexColor("#2c2c2c"),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: screenHeight * 0.06,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: screenWidth * 0.6,
-                          child: TextFormField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              labelStyle: TextStyle(
-                                color: HexColor("#2c2c2c"),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
+                      SizedBox(
+                        height: screenHeight * 0.06,
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: screenWidth * 0.6,
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                labelStyle: TextStyle(
                                   color: HexColor("#2c2c2c"),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: HexColor("#2c2c2c"),
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: screenWidth * 0.04,
-                        ),
-                        SizedBox(
-                          height: screenHeight * 0.07,
-                          width: screenWidth * 0.2,
-                          child: TextButton(
-                            onPressed: () {},
+                          SizedBox(
+                            width: screenWidth * 0.04,
+                          ),
+                          SizedBox(
+                            height: screenHeight * 0.07,
+                            width: screenWidth * 0.2,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Ok',
+                                style: GoogleFonts.josefinSans(
+                                  textStyle: TextStyle(
+                                    color: HexColor("#ffffff"),
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              style: TextButton.styleFrom(
+                                backgroundColor: HexColor("#0b85ff"),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.02,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/forgotPassword');
+                            },
                             child: Text(
-                              'Ok',
+                              'Forgot Password?',
                               style: GoogleFonts.josefinSans(
                                 textStyle: TextStyle(
-                                  color: HexColor("#ffffff"),
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
+                                  color: HexColor("#2c2c2c"),
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w600,
                                 ),
                               ),
                             ),
                             style: TextButton.styleFrom(
-                              backgroundColor: HexColor("#0b85ff"),
+                              backgroundColor: HexColor("#ffffff"),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: screenHeight * 0.02,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/forgotPassword');
-                          },
-                          child: Text(
-                            'Forgot Password?',
-                            style: GoogleFonts.josefinSans(
-                              textStyle: TextStyle(
-                                color: HexColor("#2c2c2c"),
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          style: TextButton.styleFrom(
-                            backgroundColor: HexColor("#ffffff"),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.16,
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signup');
-                },
-                child: Text(
-                  'Create a New Account',
-                  style: GoogleFonts.josefinSans(
-                    textStyle: TextStyle(
-                      color: HexColor("#2c2c2c"),
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w700,
-                    ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                style: TextButton.styleFrom(
-                  backgroundColor: HexColor("#ffffff"),
+                SizedBox(
+                  height: screenHeight * 0.16,
                 ),
-              ),
-            ],
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/signup');
+                  },
+                  child: Text(
+                    'Create a New Account',
+                    style: GoogleFonts.josefinSans(
+                      textStyle: TextStyle(
+                        color: HexColor("#2c2c2c"),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: HexColor("#ffffff"),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
