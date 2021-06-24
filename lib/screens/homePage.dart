fa294b031bac5c290ff93c69f3b230645d1137ee
handuah <handuah@gmail.com>
2021-06-24 16:19:03 +0000

    added listview

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -132,83 +132,155 @@ class _MainPageState extends State<MainPage> {
               ],
             ),
             SizedBox(
-              height: screenHeight * 0.02,
+              height: screenHeight * 0.01,
             ),
-            Stack(
-              children: [
-                Container(
-                  height: screenHeight * 0.2,
-                  width: screenWidth,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          'https://lh3.googleusercontent.com/proxy/DtLNoDJm-i4MSo5J2q2sL8K6AG8iLLSfn0_SW_G0JK3K_og39UujOb_l-QVn6Jb2PJmVwcfaxyIH-a9jwQG5EtyhsQ'),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
+            Container(
+              height: screenHeight * 0.6,
+              width: screenWidth,
+              // color: Colors.red,
+              child: ListView(
+                padding: EdgeInsets.only(top: screenHeight * 0.01),
+                children: [
+                  CategoryCards(
+                    'https://lh3.googleusercontent.com/proxy/DtLNoDJm-i4MSo5J2q2sL8K6AG8iLLSfn0_SW_G0JK3K_og39UujOb_l-QVn6Jb2PJmVwcfaxyIH-a9jwQG5EtyhsQ',
+                    'Hotels',
+                    '92',
+                    '#B54351',
                   ),
-                ),
-                Container(
-                  height: screenHeight * 0.2,
-                  width: screenWidth,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Colors.transparent, HexColor("#B54351")],
-                      // colors: [
-                      //   HexColor("#B54351"),
-                      //   Colors.transparent,
-                      //   Colors.transparent,
-                      //   HexColor("#B54351"),
-                      // ],
-                      // stops: [0.0, 0.4, 0.6, 1],
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
+                  SizedBox(
+                    height: screenHeight * 0.01,
                   ),
-                ),
-                Positioned(
-                  top: screenHeight * 0.14,
-                  left: screenWidth * 0.06,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Hotels',
-                        style: GoogleFonts.josefinSans(
-                          textStyle: TextStyle(
-                            color: HexColor("#ffffff"),
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: screenWidth * 0.02,
-                      ),
-                      CircleAvatar(
-                        maxRadius: 14.0,
-                        backgroundColor: Colors.white,
-                        child: Text(
-                          '92',
-                          style: GoogleFonts.josefinSans(
-                            textStyle: TextStyle(
-                              color: HexColor("#2c2c2c"),
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                  CategoryCards(
+                    'https://lh3.googleusercontent.com/proxy/DtLNoDJm-i4MSo5J2q2sL8K6AG8iLLSfn0_SW_G0JK3K_og39UujOb_l-QVn6Jb2PJmVwcfaxyIH-a9jwQG5EtyhsQ',
+                    'Hotels',
+                    '92',
+                    '#B54351',
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: screenHeight * 0.01,
+                  ),
+                  CategoryCards(
+                    'https://lh3.googleusercontent.com/proxy/DtLNoDJm-i4MSo5J2q2sL8K6AG8iLLSfn0_SW_G0JK3K_og39UujOb_l-QVn6Jb2PJmVwcfaxyIH-a9jwQG5EtyhsQ',
+                    'Hotels',
+                    '92',
+                    '#B54351',
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.01,
+                  ),
+                  CategoryCards(
+                    'https://lh3.googleusercontent.com/proxy/DtLNoDJm-i4MSo5J2q2sL8K6AG8iLLSfn0_SW_G0JK3K_og39UujOb_l-QVn6Jb2PJmVwcfaxyIH-a9jwQG5EtyhsQ',
+                    'Hotels',
+                    '92',
+                    '#B54351',
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class CategoryCards extends StatefulWidget {
+  // const CategoryCards({ Key? key }) : super(key: key);
+  final String imagePath;
+  final String categoryName;
+  final String categoryNumber;
+  // final String topGradient;
+  final String bottomGradient;
+
+  const CategoryCards(this.imagePath, this.categoryName, this.categoryNumber,
+      this.bottomGradient);
+
+  @override
+  _CategoryCardsState createState() => _CategoryCardsState();
+}
+
+class _CategoryCardsState extends State<CategoryCards> {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Container(
+          height: screenHeight * 0.2,
+          width: screenWidth,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              // image: NetworkImage(
+              //     'https://lh3.googleusercontent.com/proxy/DtLNoDJm-i4MSo5J2q2sL8K6AG8iLLSfn0_SW_G0JK3K_og39UujOb_l-QVn6Jb2PJmVwcfaxyIH-a9jwQG5EtyhsQ'),
+              image: NetworkImage(widget.imagePath),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        Container(
+          height: screenHeight * 0.2,
+          width: screenWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter, end: Alignment.bottomCenter,
+              // colors: [Colors.transparent, HexColor("#B54351")],
+              colors: [
+                Colors.transparent,
+                HexColor(widget.bottomGradient),
+              ],
+              // colors: [
+              //   HexColor("#B54351"),
+              //   Colors.transparent,
+              //   Colors.transparent,
+              //   HexColor("#B54351"),
+              // ],
+              // stops: [0.0, 0.4, 0.6, 1],
+            ),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        Positioned(
+          top: screenHeight * 0.14,
+          left: screenWidth * 0.06,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                // 'Hotels',
+                widget.categoryName,
+                style: GoogleFonts.josefinSans(
+                  textStyle: TextStyle(
+                    color: HexColor("#ffffff"),
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: screenWidth * 0.02,
+              ),
+              CircleAvatar(
+                maxRadius: 14.0,
+                backgroundColor: Colors.white,
+                child: Text(
+                  // '92',
+                  widget.categoryNumber,
+                  style: GoogleFonts.josefinSans(
+                    textStyle: TextStyle(
+                      color: HexColor("#2c2c2c"),
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
