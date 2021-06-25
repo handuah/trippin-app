4ae0457f26e0961250a555f82b22f2d785fa5bde
handuah <handuah@gmail.com>
2021-06-25 13:00:03 +0000

    initial commit

---
+++ lib/screens/homePageContainer.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trippin_app/screens/homePage.dart';
+
+class HomePageContainer extends StatefulWidget {
+  // const HomePageContainer({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageContainerState createState() => _HomePageContainerState();
+}
+
+class _HomePageContainerState extends State<HomePageContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      extendBody: true,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(
+            40.0,
+          ),
+          topRight: Radius.circular(
+            40.0,
+          ),
+        ),
+        child: BottomNavigationBar(
+          elevation: 4.0,
+          backgroundColor: HexColor("#F4F4F4"),
+          items: [
+            BottomNavigationBarItem(
+              // backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.home,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+              label: '',
+              // title: Text('Dashboard'),
+              activeIcon: Icon(
+                Icons.home,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+            ),
+            BottomNavigationBarItem(
+              // backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.bookmark_outline,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+              label: '',
+              // title: Text('Dashboard'),
+              activeIcon: Icon(
+                Icons.bookmark_outline,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+            ),
+            BottomNavigationBarItem(
+              // backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.notification_important_outlined,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+              label: '',
+              // title: Text('Dashboard'),
+              activeIcon: Icon(
+                Icons.notification_important_outlined,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+            ),
+            BottomNavigationBarItem(
+              // backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.person,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+              label: '',
+              // title: Text('Dashboard'),
+              activeIcon: Icon(
+                Icons.person,
+                color: HexColor('#0b85ff'),
+                size: 26.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: PageView(
+        children: [
+          MainPage(),
+          MainPage(),
+          MainPage(),
+          MainPage(),
+        ],
+      ),
+    );
+  }
+}
